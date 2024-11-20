46e608341eca5cb7227c85ed4ebba5e4736f5711
shakibfarzan <shakib@greenhabitat.at>
2024-11-21 00:24:46 +0330

    feat: add Google and GitHub sign in

--- app/(auth)/log-in/page.tsx
+++ app/(auth)/log-in/page.tsx
@@ -1,7 +1,21 @@
 import React from "react";
 
-const LoginPage = () => {
-  return <div />;
+import { auth, signIn, signOut } from "@/auth";
+
+const LoginPage = async () => {
+  const session = await auth();
+
+  return (
+    <form
+      action={async () => {
+        "use server";
+        if (!session?.user) await signIn("github");
+        else await signOut();
+      }}
+    >
+      <button type="submit">Signin with GitHub</button>
+    </form>
+  );
 };
 
 export default LoginPage;
---
+++ auth.ts
@@ -0,0 +1,30 @@
+import NextAuth from "next-auth";
+import Google from "next-auth/providers/google";
+import GitHub from "next-auth/providers/github";
+
+export const { handlers, signIn, signOut, auth } = NextAuth({
+  providers: [
+    Google({
+      clientId: process.env.AUTH_GOOGLE_ID,
+      clientSecret: process.env.AUTH_GOOGLE_SECRET,
+      authorization: {
+        params: {
+          prompt: "consent",
+          access_type: "offline",
+          response_type: "code",
+        },
+      },
+    }),
+    GitHub({
+      clientId: process.env.AUTH_GITHUB_ID,
+      clientSecret: process.env.AUTH_GITHUB_SECRET,
+      authorization: {
+        params: {
+          prompt: "consent",
+          access_type: "offline",
+          response_type: "code",
+        },
+      },
+    }),
+  ],
+});
